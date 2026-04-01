c578ca546dcd253a9c3bdd48b368933ceb2c025e
nitin chaudhary <nitincatt@gmail.com>
2026-04-01 14:31:53 +0530

    setup backend with Express, Mongoose, and basic routes for doctors and patients

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+// required dependencies
+const express = require('express');
+const connectToDatabase = require('./connection');
+const homeRoutes = require('./routes/homeRoutes');
+const path = require('path');
+
+//port and app setup
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, '..', 'frontend')));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'home_dashboard'))); // serve frontend files
+//db connection
+connectToDatabase('mongodb://localhost:27017/mydatabase').then(() => console.log('Connected to MongoDB')).catch(err => console.error('Failed to connect to MongoDB', err));
+
+//routes
+app.use('/', homeRoutes);
+
+
+
+app.listen(port);
---
+++ backend/routes/homeRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, '..', '..', 'frontend', 'home_dashboard', 'index.html')
+  );
+});
+
+module.exports = router;
